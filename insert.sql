33721b78ecb5e36518c3553f6befaee41b46a4d4
Marcos <marcosviniciohd@hotmail.com>
2021-07-13 17:30:27 -0300

    insert na tabela de cliente

---
+++ insert.sql
@@ -0,0 +1,10 @@
+use ecommerce;
+
+select * from cliente;
+
+insert into cliente (codigo, nome, tipo_pessoa) values (1, 'Marcos Vinício', 'F');
+insert cliente (codigo, nome, tipo_pessoa) values (2, 'Daniela Oliveira', 'F');
+insert cliente (tipo_pessoa, codigo, nome) values ('J', 3, 'Natalício Campos Jr');
+insert cliente values (4, 'Marília Novais', 'F');
+insert cliente values (5, 'Hewyllen Aparecida', 'J'), (4, 'Dorotéia Campos', 'J');
+
